5a2a7a7cfa8bb808aa750e4a925940409431c2e1
Huang jr <jr@nlpweb.org>
2015-10-04 22:52:28 +0800

    homework

--- flip_flap.rb
+++ flip_flap.rb
@@ -2,5 +2,12 @@ require_relative 'tsv_buddy'
 
 class FlipFlap
   # Do NOT create an initialize method
+def take_yaml(yml)
+    require 'yaml'
+    @data = YAML::load(yml)
+end
 
+def to_yaml
+    @data.to_yaml
+end
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,31 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
-  end
+    info = []
+    @data = []
+    programmer = {}
+    file = tsv.split("\n")
+    first_line = file.first
+    info = first_line.split("\t")
+    file.shift
+    file.each{|x| @data << info.zip(x.split("\t")).to_h }
+    @data
+ end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    info = @data[0].keys
+    result = []
+    result << info.join("\t")
+    line = []
+    @data.each do |dict|
+      dict.each do | _, value|
+        line << value
+      end
+      result << line.join("\t")
+      line = []
+    end
+    result
   end
 end
